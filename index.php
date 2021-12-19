cdd0456828f91ec4921353639d60971575ab9610
Eli Griv <47922495+3ligriv@users.noreply.github.com>
2021-12-19 17:55:40 +0100

    #1 Added draft index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Some random site, not finished.">
+        <meta name="theme-color" content="#202020">
+        <title>Ilmenite.org</title>
+        <link rel="stylesheet" type="text/css" href="css/desktop.css">
+    </head>
+
+    <body>
+        <header>
+            <h1><a href="index.php">There is nothing here</a></h1>
+            <h2>Come back later</h2>
+        </header>
+        
+        <main>
+            <canvas>This website requires javascript.</canvas>
+        </main>
+        
+        <footer>Virgile Schwey - 2021-12-19</footer>
+    </body>
+</html>
